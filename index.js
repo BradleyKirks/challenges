484633514b407bb8b656fe7e3969a7cd88a78791
Bradley Kirks <spinnyorb23@aim.com>
2023-10-10 08:36:14 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function hello(name){
+    const message = "hello "
+    return message + name
+}
+
+
+console.log(hello("Brad"))
+
+function multiply(num1, num2){
+    const product = num1 * num2
+    return num1 + " x " + num2 + ' = ' + product
+}
+
+
+console.log(multiply(23,3))
+console.log(multiply(2,6))
